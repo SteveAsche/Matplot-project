ba6cc0f3093df932eac13e7fe992d849e85217be
SteveAsche <steve.asche@gmail.com>
2018-12-04 18:53:49 -0800

    Add files via upload
    updated with sorting

--- dice_roller.py
+++ dice_roller.py
@@ -23,7 +23,8 @@ def check_win(sumup, point_on, point_set=0):
 	return win
 
 
-
+def takeSecond(elem):
+	return elem[1]
 
 
 #Create a randomwalk set
@@ -45,6 +46,7 @@ point_set = 0
 lose_count = 0
 win_count = 0
 pass_count = 0
+money_pass = 0
 
 
 	#Set the size of the plotting window
@@ -56,13 +58,15 @@ for i in range(len(dicrole.die1)):
 	outcome = check_win(sumup, point_on, point_set)
 
 
-	if not point_on:
+	if not point_on: #if the point is not on, the point will stay off if the roll is 2, 3, 12, 7
 		if sumup == 2 or sumup == 3 or sumup == 12 or sumup == 7:
 			point_set = 0
 			point_on = False
 		else:
 			point_set = sumup
 			point_on = True
+	#the outcome of the roll is based on whether the point is on or off 
+
 
 	if outcome == "lose":
 		lose_count += 1
@@ -74,11 +78,16 @@ for i in range(len(dicrole.die1)):
 		point_set = 0
 	else:
 		pass_count += 1
+		if sumup == 2 or sumup == 3 or sumup == 12 or sumup == 11:
+			pass
+		else:
+			money_pass +=1
 
-
-
-
+	#numset is the list that tallies the number of times a given number comes up
 	numset[(sumup-2)] += 1
+
+	#a 7 ends the streak no matter what
+	
 	if sumup == 7:
 		if streak_count in streak_dict:
 			streak_dict[streak_count] += 1
@@ -132,15 +141,43 @@ print(streak_dict)
 print("Wins: " + str(win_count))
 print("Losses: " + str(lose_count))
 print("Passes: " + str(pass_count))
+print("Field payoffs: " + str(money_pass))
 
 print("Total Rolls: " + str(win_count + lose_count + pass_count))
 
 roll_listy = list(streak_dict.values())
 roll_listx = list(streak_dict.keys())
 
+#Zip the lists ans sort them 
+
+
+zipped_lists = list(zip(roll_listx, roll_listy))
+print(zipped_lists)
+
+#sorted(zipped_lists, key=takeSecond, reverse=True)
+new_zippy = sorted(zipped_lists, key = lambda x: x[0])
+new_zippy = sorted(new_zippy, key=lambda x: x[1], reverse = True)
+
+print(new_zippy)
+
+holdlistx = []
+holdlisty = []
+
+for i in new_zippy:
+	holdlistx.append(i[0])
+	holdlisty.append(i[1])
+
+print(holdlistx)
+print(holdlisty)
+
+
+
 roll_listx_string = []
-for i in roll_listx:
+for i in holdlistx:
 	roll_listx_string.append(str(i))
+
+#Zip the lists ans sort them 
+
 
 
 print(roll_listy)
@@ -155,7 +192,7 @@ hist.x_labels = roll_listx_string
 hist.x_title = "Length of streak"
 hist.y_title = "Occurrence of streak"
 
-hist.add('Streaks', roll_listy)
+hist.add('Streaks', holdlisty)
 hist.render_to_file('streak_vidual.svg')
 
 
